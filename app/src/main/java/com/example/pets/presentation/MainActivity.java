ac14c1d111f26c231cbf44100f2502166f2c45b4
darkharov <dennis.arkharov@gmail.com>
2019-02-02 15:02:52 +0200

    initial commit

---
+++ app/src/main/java/com/example/pets/presentation/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.pets.presentation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.pets.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
